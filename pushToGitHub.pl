4e376e35631423c406548d04b2b92a4306d15b96
Philip R Brenan <philiprbrenan@gmail.com>
2024-08-18 21:02:09 +0100

    Description of this repository

--- pushToGitHub.pl
+++ pushToGitHub.pl
@@ -10,12 +10,12 @@ yyy(<<END);
 git add  action.yml
 git commit -m "The packaged action"
 git add .github/workflows/test.xml
-git commit -m "An example of using the packaged"
+git commit  --amend  -m "An example of using the packaged"
 git add hello.py
-git commit -m "The code executed by the packaged action"
+git commit  --amend -m "The code executed by the packaged action"
 git add pushToGitHub.pl
-git commit -m "Code to auto upload the files to github"
+git commit  --amend -m "Code to auto upload the files to github"
 git add README.md
-git commit -m "Description of this repository"
+git commit  --amend -m "Description of this repository"
 git push
 END
