0a1b14a85dbabfe9c1ffead6992a32aa3394ad98
Philip R Brenan <philiprbrenan@gmail.com>
2024-08-18 20:51:15 +0100

    aaaa

--- pushToGitHub.pl
+++ pushToGitHub.pl
@@ -3,17 +3,9 @@
 # Push testPackage to GitHub
 # Philip R Brenan at gmail dot com, Appa Apps Ltd Inc., 2024
 #-------------------------------------------------------------------------------
-use warnings FATAL => qw(all);
-use strict;
-use Carp;
-use Data::Dump qw(dump);
+use v5.34;
 use Data::Table::Text qw(:all);
-use GitHub::Crud qw(:all);
-use feature qw(say current_sub);
 
-my $home = currentDirectory;                                                    # Local files
-my $user = q(philiprbrenan);                                                    # User
-my $repo = q(testPackage);                                                      # Repo
 yyy(<<END);
 git add *
 git commit -m "aaaa"
